9ed220b6469e612622d7e85614814c82740522a7
Seth Shaw <sshaw28@asu.edu>
2024-03-07 20:16:30 +0000

    add conditions to analytics query service; add todos to README

--- src/Service/AnalyticsQueryService.php
+++ src/Service/AnalyticsQueryService.php
@@ -50,34 +50,76 @@ class AnalyticsQueryService {
   }
 
   /**
-   * Returns resource_engagement for nodes by month.
+   * Returns event counts for events for an entity by month.
    *
    * @param \Drupal\Core\Entity\EntityInterface $entity
    *   The entity to gather analytics on.
-   * @param array $event
-   *   Events used for reporting. Leave empty for all events.
+   * @param array $events
+   *   Events used for reporting. Leave empty for all events (default).
    * @param string $start_date
-   *   The start date in 'YYYY-mm-dd' format or 'yesterday' or 'today'.
+   *   The start date in 'YYYY-mm' format.
    * @param string $end_date
-   *   The end date in 'YYYY-mm-dd' format or 'yesterday' or 'today'.
+   *   The end date in 'YYYY-mm' format.
    *
    * @return array
    *   Array of event counts keyed by month (YYYY-mm).
+   *
+   * @throws Exception
+   *   If a function parameter is invalid.
    */
-  public function entityMonthly($entity, $event = [], $start_date = '', $end_date = '') {
-    // @todo query the item_analytic_counts table.
-    // Validation
+  public function entityMonthly($entity, $events = [], $start_date = '', $end_date = '') {
+    // Validation.
     if (!$entity instanceof EntityInterface) {
       throw new \Exception("Received a " . gettype($entity) . " instead of an entity.");
     }
+    if (!is_array($events)) {
+      throw new \Exception("Events parameter must be an array.");
+    }
+
+    // Query.
     $query = $this->connection->select('item_analytic_counts', 'iac')
-      ->fields('iac', ['period', 'count'])
+      ->fields('iac', ['period'])
       ->condition('iac.iid', $entity->id())
-      ->condition('iac.type', $entity->getEntityTypeId());
-    // @todo Add event condition if provided a non-empty event array.
-    // @todo Add start and end date if valid values provided.
-    $data = $query->execute()->fetchAllKeyed();
-    return $data;
+      ->condition('iac.type', $entity->getEntityTypeId())
+      ->groupBy('iac.period');
+    $query->addExpression('sum(iac.count)', 'count');
+
+    // Add event condition if provided a non-empty event array.
+    if (!empty($events)) {
+      $query->condition('iac.event', $events, 'IN');
+    }
+
+    // Add start and end date if valid values provided.
+    if (!empty($start_date) && $this->validateDate($start_date)) {
+      $query->condition('iac.period', $start_date, '>=');
+    }
+    if (!empty($end_date) && $this->validateDate($end_date)) {
+      $query->condition('iac.period', $end_date, '<=');
+    }
+
+    // Make it so.
+    return $query->execute()->fetchAllKeyed();
+  }
+
+  /**
+   * Ensures date string matches 'YYYY-mm'.
+   *
+   * @param string $date
+   *   The date value to validate.
+   *
+   * @return bool
+   *   True if it validates
+   *
+   * @throws Exception
+   *   If the date is invalid.
+   */
+  private function validateDate($date) {
+    $m = [];
+    if (!preg_match('/^\d{4}-(\d{2})$/', $date, $matches) || $matches[1] > 12 || $matches[1] < 1) {
+      throw new \Exception("Invalid period '$date'. Period must match the pattern 'YYYY-mm', e.g. '2024-01', with a month value between 1 and 12.");
+    }
+    return TRUE;
+
   }
 
 }
