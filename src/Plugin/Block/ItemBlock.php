7afd2ad6c473ce03540b6af9ff1bfe7c40aeb65a
Seth Shaw <sshaw28@asu.edu>
2024-10-16 15:30:10 -0700

    Update Popover for Bootstrap 5

--- src/Plugin/Block/ItemBlock.php
+++ src/Plugin/Block/ItemBlock.php
@@ -87,17 +87,19 @@ class ItemBlock extends BlockBase implements ContainerFactoryPluginInterface {
     $count = array_sum($this->analyticsQueryService->entityMonthly($entity) ?? []);
     if ($count > 0) {
       return [
+        '#type' => 'container',
+        '#attributes' => ['class' => ['asu-item-analytics-popover']],
         'popover' => [
           '#type' => 'html_tag',
           '#tag' => 'span',
           '#attributes' => [
             'tabindex' => '0',
             'role' => 'button',
-            'data-placement' => 'bottom',
-            'data-toggle' => 'popover',
-            'data-trigger' => 'focus',
+            'data-bs-placement' => 'bottom',
+            'data-bs-toggle' => 'popover',
+            'data-bs-trigger' => 'focus',
             'title' => 'Information',
-            'data-content' => 'The repository began collecting download statistics in 2021.',
+            'data-bs-content' => 'The repository began collecting download statistics in 2021.',
           ],
           'icon' => [
             '#type' => 'html_tag',
@@ -110,7 +112,7 @@ class ItemBlock extends BlockBase implements ContainerFactoryPluginInterface {
           '#plain_text' => 'Download count: ' . number_format($count),
         ],
         // Attach the library to the block.
-        '#attached' => ['library' => ['asu_item_analytics/item_block' => 'asu_item_analytics/item_block']],
+        '#attached' => ['library' => ['asu_item_analytics/item_block']],
         '#cache' => ['contexts' => ['url.path']],
       ];
     }
