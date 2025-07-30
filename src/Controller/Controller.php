00664c711ee04ec4c589560e872b2f40fc3e71bc
Seth Shaw <sshaw28@asu.edu>
2025-07-30 15:56:05 +0000

    ignore staff roles for analytics

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -3,11 +3,12 @@
 namespace Drupal\asu_item_analytics\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Drupal\asu_item_analytics\Service\GoogleAnalyticsQueryService;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\asu_item_analytics\Service\AnalyticsQueryService;
 use Drupal\asu_item_analytics\Service\AnalyticsUpdateService;
-use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\asu_item_analytics\Service\GoogleAnalyticsQueryService;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * Provides a controller for ASU Item Analytics module.
@@ -35,10 +36,18 @@ class Controller extends ControllerBase implements ContainerInjectionInterface {
    */
   protected AnalyticsUpdateService $itemUpdate;
 
-  public function __construct(GoogleAnalyticsQueryService $gaQuery, AnalyticsQueryService $itemQuery, AnalyticsUpdateService $itemUpdate) {
+  /**
+   * The logger channel.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
+
+  public function __construct(GoogleAnalyticsQueryService $gaQuery, AnalyticsQueryService $itemQuery, AnalyticsUpdateService $itemUpdate, LoggerInterface $logger) {
     $this->gaQuery = $gaQuery;
     $this->itemQuery = $itemQuery;
     $this->itemUpdate = $itemUpdate;
+    $this->logger = $logger;
   }
 
   /**
@@ -48,7 +57,8 @@ class Controller extends ControllerBase implements ContainerInjectionInterface {
     return new static(
       $container->get('asu_item_analytics.ga_query'),
       $container->get('asu_item_analytics.query'),
-      $container->get('asu_item_analytics.update')
+      $container->get('asu_item_analytics.update'),
+      $container->get('logger.factory')->get('asu_item_analytics')
     );
   }
 
@@ -76,6 +86,22 @@ class Controller extends ControllerBase implements ContainerInjectionInterface {
    *   The JSON response with the updated count.
    */
   public function increment($node) {
+    $user = $this->currentUser();
+    $roles = $user->getRoles();
+
+    // Ignore counts from repository staff and administrators.
+    $matched_roles = array_intersect($roles, [
+      'administrator', 'collections_staff',
+      'repositor_manager', 'content_approver',
+      'metadata_manager', 'preservationist',
+    ]);
+    if (count($matched_roles) > 0) {
+      $message = "Ignoring item analytics increment for {$user->getAccountName()}'s roles: " . implode(', ', $matched_roles);
+      $this->logger->info($message);
+      return new JsonResponse(['message' => $message]);
+    }
+
+    // Increment the resource engagement count for the current month.
     $current_month = date('Y-m');
     $current_counts = $this->itemQuery->entityMonthly($node, ['resource_engagement'], $current_month, $current_month);
     $current_count = (array_key_exists($current_month, $current_counts)) ? $current_counts[$current_month] : 0;
