88629bbdc68818755dee9b999b6743f8a8fb079f
Seth Shaw <108362375+seth-shaw-asu@users.noreply.github.com>
2025-10-22 13:08:23 -0700

    Clean up download link click event handler
    Removed console log and debugging event prevention from download link click handler.

--- js/download-counter.js
+++ js/download-counter.js
@@ -15,16 +15,12 @@
 
       links.forEach(function (el) {
         el.addEventListener('click', function (event) {
-          console.log('Download link clicked:', el.href);
-
           if (Drupal.asuItemAnalytics && typeof Drupal.asuItemAnalytics.incrementIfNeeded === 'function') {
             Drupal.asuItemAnalytics.incrementIfNeeded();
           } else {
             // Defensive logging if the shared library wasn't loaded.
             console.warn('asuItemAnalytics not available - make sure the shared library is attached.');
           }
-                  // Debugging:
-        event.preventDefault();
         });
       });
     }
